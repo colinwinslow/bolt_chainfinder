e63652e2aee154da1f2cab5b6473b2698cbaf773
Colin Winslow <colinwinslow@gmail.com>
2012-07-04 10:23:26 -0700

    Fixed (all?) the bugs in search algorithm, simplified cost function for the time being.

--- chainfinder.py
+++ chainfinder.py
@@ -5,38 +5,62 @@ quick description and documentation in attached readme file.
 '''
 import util
 import numpy as np
-from math import sqrt
 import heapq
-
+from collections import namedtuple
 global distance_limit 
 # sets the allowed range for variance in spacing from one pair of objects in the line to the next.
-# if abs(ln(x)) rises above this limit, the object is not considered as a valid successor state
-# by the pathfinding algorithm.
-distance_limit = 1
+# a value of 1 allows objects to range from half as distant to twice as distant, and a value of 2
+# allows them to range from 1/4 as distant to 4 times as distant, etc.
+distance_limit = 105
 
 
 global angle_limit 
 #the max angle, in radians, that an object is allowed to deviate from the existing line.
 angle_limit = .7
 
-
 global min_line_length
 #minimum number of objects considered to be a line
 min_line_length=3
 
-global angular_weight,distance_weight
+global anglevar_weight,distvar_weight,dist_weight
 #adjusts the weight of angle and distance variation in the cost function.
-angular_weight = 1
-distance_weight = 1
+anglevar_weight = 0
+distvar_weight = 0
+dist_weight=1
 
 def main():
+    
+    print "Sample run of line detecton on Blockworld: \n"
     np.seterr(all='raise')
     
     print "scene 14, step 8"
-    findChains(util.get_objects(14, 8)) #final scene
+    findChains(util.get_objects(14, 8))
     print""
     print "scene 4, step 5"
-    findChains(util.get_objects(4, 5)) #14th folder
+    findChains(util.get_objects(4, 5))
+    
+    PhysicalObject = namedtuple('physicalObject', ['id', 'position', 'bbmin', 'bbmax'])
+#    
+#    a = PhysicalObject(1,np.array([1,0]),1,1)
+#    b = PhysicalObject(2,np.array([2,0]),1,1)
+#    c = PhysicalObject(3,np.array([3,0]),1,1)
+#    d = PhysicalObject(4,np.array([4,0]),1,1)
+#    e = PhysicalObject(5,np.array([5,0]),1,1)
+#    
+#    f = PhysicalObject(6,np.array([1,1]),1,1)
+#    g = PhysicalObject(7,np.array([2,2]),1,1)
+#    h = PhysicalObject(8,np.array([3,3]),1,1)
+#    i = PhysicalObject(9,np.array([4,4]),1,1)
+#    j = PhysicalObject(10,np.array([5,5]),1,1)
+#    k = PhysicalObject(11,np.array([6,6]),1,1)
+#
+#    
+#    print"test problem"
+#    findChains([a,b,c,d,e,f,g,h,i,j,k])
+    
+    
+    
+    
     
 
 def findChains(inputObjectSet):
@@ -45,50 +69,41 @@ def findChains(inputObjectSet):
     
     bestlines = []
     for pair in startingPairs:
-        bestlines.append(chainSearch(pair[0], pair[1], inputObjectSet))
+        result = chainSearch(pair[0], pair[1], inputObjectSet, trace)
+        if result != None: bestlines.append(result)
+    print "bestlines: ",bestlines
     print "best lines:"
     verybest = []
     for line in bestlines:
-        if len(line)>=min_line_length+1:
+        if len(line)>min_line_length:
             verybest.append(line)
-    verybest.sort(key=lambda list: list[len(line)-1])#something isn't right here...
+    verybest.sort(key=lambda l: l[len(l)-1])
+    
     for line in verybest:
-         print  "cost: ", line[len(line)-1],util.lookup_objects(line)
+        print  "cost: ", line[len(line)-1],"\t",util.lookup_objects(line)
     return verybest
 
         
             
-def chainSearch(start, finish, points):
+def chainSearch(start, finish, points, trace=False):
     node = Node(start, -1, [], 0)
     frontier = PriorityQueue()
-    fringeDirectory = {util.totuple(start):node}
     frontier.push(node, 0)
     explored = set()
-    loopcount = 0
     while frontier.isEmpty() == False:
-#        loopcount+=1
-#        print "popping frontier #",loopcount
         node = frontier.pop()
-        frontier.isEmpty()
-        try:fringeDirectory.pop(util.totuple(node.getState()))
-        except: pass
         if node.getState().id == finish.id:
             path = node.traceback()
             path.insert(0, start.id)
             return path
-        explored.add(util.totuple(node.getState()))
-        for action in node.getSuccessors(points):
-            child = action
-            if util.totuple(child.state) not in explored and util.totuple(child.getState()) not in fringeDirectory:
-#                    print"pushing to frontier", child.state.id
-                    frontier.push(child, child.cost)
-                    fringeDirectory[util.totuple(child.getState())] = child
-            elif util.totuple(child.getState()) in fringeDirectory:
-                
-                if fringeDirectory[util.totuple(child.getState())].cost > child.cost:
-#                    print fringeDirectory[util.totuple(child.getState())].cost, child.cost
-                    frontier.push(child,child.cost)
-                    fringeDirectory[util.totuple(child.getState())] = child
+        explored.add(node.state.id)
+        successors = node.getSuccessors(points,start,finish)
+        for child in successors:
+            if child.state.id not in explored and frontier.contains(child.state.id)==False:
+                frontier.push(child, child.cost)
+            elif frontier.contains(child.state.id) and frontier.pathCost(child.state.id) > child.cost:
+                frontier.push(child,child.cost)
+
                 
             
         
@@ -100,25 +115,36 @@ def chainSearch(start, finish, points):
             
         
         
-#def search(points, start, goal):
-    
-def angleCost(a, b, c):
+#cost functions
+def oldAngleCost(a, b, c):
     '''angle cost of going to c given we came from ab'''
     abDir = b - a
     bcDir = c - b
     difference = util.findAngle(abDir, bcDir)
     if np.isnan(difference): return 0
     else: return np.abs(difference)
-def distanceCost(a, b, c):
+def angleCost(a, b, c, d):
+    '''prefers straighter lines'''
+    abDir = b - a
+    cdDir = d - c
+    difference = util.findAngle(abDir, cdDir)
+    if np.isnan(difference): return 0
+    else: return np.abs(difference)
+def distVarCost(a, b, c):
     #np.seterr(all='warn')
-    '''treats longer or shorter distances with equal disdain'''
+    '''prefers lines with less variance in their spacing'''
     abDist = util.findDistance(a, b)
     bcDist = util.findDistance(b, c)
     if bcDist==0:
         return 200
-    almostdone= np.abs(np.log((1/abDist)*bcDist))
-
+    almostdone= np.abs(np.log2((1/abDist)*bcDist))
     return almostdone
+def distCost(current,step,start,goal):
+    '''prefers dense lines to sparse ones'''
+    stepdist = util.findDistance(current, step)
+    totaldist= util.findDistance(start, goal)
+    return stepdist**2/totaldist**2
+    
     
 
 
@@ -128,46 +154,57 @@ class Node:
         self.state = state
         self.parent = parent
         self.action = action
-        self.cost = cost
-#        if parent != -1:
-#            self.cost = parent.cost + cost
+        self.icost = cost
+        if parent != -1:
+            self.cost = parent.cost + cost
+        else: self.cost=cost
     def getState(self):
         return self.state
-    def getSuccessors(self, points):
+    
+    def getSuccessors(self, points,start,finish):
         
         
         if self.parent == -1:
             out = []
-            try: points.remove(self)
-            except: pass
             for p in points:
-                dist = util.findDistance(self.state.position, p.position)
-                out.append(Node(p,self,p.id,dist))
+                if self.state.id != p.id and finish.id!=p.id: 
+                    
+                    aCost = angleCost(self.state.position,finish.position, self.state.position, p.position)
+                    if aCost <= angle_limit:
+                        dCost =distCost(self.state.position,p.position,start.position,finish.position)
+                        if dCost < 1: # prevents it from choosing points that overshoot the target.
+                            normA = anglevar_weight*(aCost/angle_limit)
+                            normD = dist_weight*dCost
+                            finalcost = (normA+normD)/(anglevar_weight+dist_weight)
+                            out.append(Node(p,self,p.id, finalcost))
             return out
         else:
 #            print "parent is not -1"
             out = []
-            try: points.remove(self)
-            except: pass
+            
             for p in points:
                 if self.state.id != p.id: 
-                    dCost = distanceCost(self.parent.state.position, self.state.position, p.position)
-                    aCost = angleCost(self.parent.state.position, self.state.position, p.position)
-                    
-                    if aCost <= angle_limit and dCost <= distance_limit:
-                        out.append(Node(p,self,p.id,angular_weight*aCost+distance_weight*dCost))
+                    vCost = distVarCost(self.parent.state.position, self.state.position, p.position)
+                    #aCost = angleCost(self.parent.state.position, self.state.position,self.state.position, p.position)
+                    aCost = oldAngleCost(self.parent.state.position,self.state.position,p.position)
+                    dCost = distCost(self.state.position,p.position,start.position,finish.position)
+                    if aCost <= angle_limit and dCost <= 1:
+                        normV = distvar_weight*(vCost/distance_limit)
+                        normA = anglevar_weight*(aCost/angle_limit)
+                        finalCost = (normA+normV+dCost)/(distvar_weight+anglevar_weight+dist_weight)
+                        out.append(Node(p,self,p.id,finalCost))
+                        
             return out
 
     def traceback(self):
-        cost = 0
+#         cost = 0
         solution = []
         node = self
         while node.parent != -1:
             solution.insert(0, node.action)
-            cost += node.cost
             node = node.parent
-        cardinality = len(solution)
-        cost = cost/cardinality
+        cardinality = len(solution)-1 #exclude the first node, which has cost 0
+        cost = self.cost/cardinality
         solution.append(cost)
         return solution
 
@@ -177,16 +214,24 @@ class PriorityQueue:
 
     def  __init__(self):  
         self.heap = []
+        self.dict = dict()
     
     def push(self, item, priority):
         pair = (priority, item)
         heapq.heappush(self.heap, pair)
-#        print "PQ now has ", len(self.heap), "members"
+        self.dict[item.state.id]=priority
+    
+    def contains(self,item):
+        return self.dict.has_key(item)
+    
+    def pathCost(self,item):
+        return self.dict.get(item)
 
 
     def pop(self):
         (priority, item) = heapq.heappop(self.heap)
         return item
+        
   
 
     def isEmpty(self):
