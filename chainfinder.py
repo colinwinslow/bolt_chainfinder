feab7078a321e2dd5804e7a9eb7097dccc666c1d
Colin Winslow <colinwinslow@gmail.com>
2012-07-11 11:10:01 -0700

    GUI now mostly working

--- chainfinder.py
+++ chainfinder.py
@@ -6,36 +6,38 @@ quick description and documentation in attached readme file.
 import util
 import numpy as np
 import heapq
+from collections import namedtuple
 
 
 global ignore_z
 #ignores z axis data
 ignore_z = False
 
-global distance_limit 
-# sets the allowed range for variance in spacing from one pair of objects in the line to the next.
-# a value of 1 allows objects to range from half as distant to twice as distant, and a value of 2
-# allows them to range from 1/4 as distant to 4 times as distant, etc.
-distance_limit = 3
+#global distance_limit 
+## sets the allowed range for variance in spacing from one pair of objects in the line to the next.
+## a value of 1 allows objects to range from half as distant to twice as distant, and a value of 2
+## allows them to range from 1/4 as distant to 4 times as distant, etc.
+#distance_limit = 3
 
 
-global angle_limit 
-#the max angle, in radians, that an object is allowed to deviate from the existing line.
-angle_limit = .9
-
-global min_line_length
-#minimum number of objects considered to be a line
-min_line_length=3
-
-global anglevar_weight,distvar_weight,dist_weight
-#adjusts the weight of angle and distance variation in the cost function.
-
-anglevar_weight = .05     # these two need to be small compared to dist_weight
-distvar_weight = .1      
-                       
-dist_weight=1
+#global angle_limit 
+##the max angle, in radians, that an object is allowed to deviate from the existing line.
+#angle_limit = .9
 
 
+
+#global anglevar_weight,distvar_weight,dist_weight
+##adjusts the weight of angle and distance variation in the cost function.
+#
+#anglevar_weight = .05     # these two need to be small compared to dist_weight
+#distvar_weight = .1      
+#                       
+#dist_weight=1
+
+#wraps search parameters in a named tuple so they can be passed around easily
+CParams = namedtuple("ChainParams",['distance_limit', 'angle_limit', 'min_line_length',
+               'anglevar_weight', 'distvar_weight','dist_weight',
+               'mode'])
 
 
 def main():
@@ -45,8 +47,15 @@ def main():
     for line in findChains(util.get_objects(14, 8)):
         print  "prior: ", np.round(line[0],4),"\t",util.lookup_objects(line[1])
 
-def findChains(inputObjectSet):
+def findChains(inputObjectSet, 
+               distance_limit=3, angle_limit=0.9, min_line_length=3,
+               anglevar_weight= .05, distvar_weight=.1,dist_weight=1,
+               mode=1):
     '''finds all the chains, then returns the ones that satisfy constraints, sorted from best to worst.'''
+    
+    params = CParams(distance_limit, angle_limit, min_line_length,
+               anglevar_weight, distvar_weight,dist_weight,
+               mode)
     bestlines = []
     explored = set()
     skipped = 0
@@ -56,7 +65,7 @@ def findChains(inputObjectSet):
     for pair in pairwise:
         start,finish = pair[0],pair[1]
         if frozenset([start.id,finish.id]) not in explored:
-            result = chainSearch(start, finish, inputObjectSet)
+            result = chainSearch(start, finish, inputObjectSet,params)
             if result != None: 
                 bestlines.append(result)
                 s = map(frozenset,util.find_pairs(result[0:len(result)-1]))
@@ -74,7 +83,7 @@ def findChains(inputObjectSet):
     costs = map(lambda l: l.pop()/costSum,verybest)
     return zip(costs,verybest)
             
-def chainSearch(start, finish, points):
+def chainSearch(start, finish, points,params):
     node = Node(start, -1, [], 0)
     frontier = PriorityQueue()
     frontier.push(node, 0)
@@ -86,7 +95,7 @@ def chainSearch(start, finish, points):
             path.insert(0, start.id)
             return path
         explored.add(node.state.id)
-        successors = node.getSuccessors(points,start,finish)
+        successors = node.getSuccessors(points,start,finish,params)
         for child in successors:
             if child.state.id not in explored and frontier.contains(child.state.id)==False:
                 frontier.push(child, child.cost)
@@ -139,7 +148,7 @@ class Node:
     def getState(self):
         return self.state
     
-    def getSuccessors(self, points,start,finish):
+    def getSuccessors(self, points,start,finish,params):
         
         out = []
         if self.parent == -1: 
@@ -147,23 +156,23 @@ class Node:
                 if self.state.id != p.id and finish.id!=p.id: 
                     aCost = angleCost(self.state.position,finish.position, self.state.position, p.position)
                     dCost =distCost(self.state.position,p.position,start.position,finish.position)
-                    if aCost <= angle_limit and dCost < 1: # prevents it from choosing points that overshoot the target.
-                        normA = anglevar_weight*(aCost/angle_limit)
-                        normD = dist_weight*dCost
-                        finalcost = (normA+normD)/(anglevar_weight+dist_weight)
+                    if aCost <= params.angle_limit and dCost < 1: # prevents it from choosing points that overshoot the target.
+                        normA = params.anglevar_weight*(aCost/params.angle_limit)
+                        normD = params.dist_weight*dCost
+                        finalcost = (normA+normD)/(params.anglevar_weight+params.dist_weight)
                         out.append(Node(p,self,p.id, finalcost))
         else:
             out = []
             for p in points:
                 if self.state.id != p.id: 
                     vCost = distVarCost(self.parent.state.position, self.state.position, p.position)       
-                    aCost = angleCost(self.parent.state.position, self.state.position,self.state.position, p.position)
-#                    aCost = oldAngleCost(self.parent.state.position,self.state.position,p.position)
+                    if params.mode==1: aCost = angleCost(self.parent.state.position, self.state.position,self.state.position, p.position)
+                    elif params.mode==2: aCost = oldAngleCost(self.parent.state.position,self.state.position,p.position)
                     dCost = distCost(self.state.position,p.position,start.position,finish.position)
-                    if aCost <= angle_limit and dCost <= 1:
-                        normV = distvar_weight*(vCost/distance_limit)
-                        normA = anglevar_weight*(aCost/angle_limit)
-                        finalCost = (normA+normV+dCost)/(distvar_weight+anglevar_weight+dist_weight)
+                    if aCost <= params.angle_limit and dCost <= 1:
+                        normV = params.distvar_weight*(vCost/params.distance_limit)
+                        normA = params.anglevar_weight*(aCost/params.angle_limit)
+                        finalCost = (normA+normV+dCost)/(params.distvar_weight+params.anglevar_weight+params.dist_weight)
                         out.append(Node(p,self,p.id,finalCost))
                         
         return out
--- graphicalview.py
+++ graphicalview.py
@@ -3,6 +3,11 @@ from collections import namedtuple
 import chainfinder
 import numpy as np
 
+CParams = namedtuple("ChainParams",['distance_limit', 'angle_limit', 'min_line_length',
+               'anglevar_weight', 'distvar_weight','dist_weight',
+               'mode'])
+
+
 class PlaygroundWindow:
     def __init__(self,master):
         
@@ -12,31 +17,43 @@ class PlaygroundWindow:
         
         
         #algorithm parameters
-        pathfinding_mode = 0
+        self.pathfinding_mode = 1
         
         #variables
         varFrame = Frame(frame,relief=SUNKEN,borderwidth=2,width=2,)
+        self.distance_limit=StringVar()
+        self.distance_limit.set("3")
+        self.angle_limit=StringVar()
+        self.angle_limit.set(".9")
+        self.min_line_length=StringVar()
+        self.min_line_length.set("3")
+        self.anglevar_weight=StringVar()
+        self.anglevar_weight.set(".1")
+        self.distvar_weight=StringVar()
+        self.distvar_weight.set(".1")
+        self.dist_weight=StringVar()
+        self.dist_weight.set("1")
         
-        pmode_button_lines = Radiobutton(varFrame,text="Lines",variable=pathfinding_mode,value=1).pack(anchor=W)
-        pmode_button_chains = Radiobutton(varFrame,text="Chains",variable=pathfinding_mode,value=2).pack(anchor=W)
+        pmode_button_lines = Radiobutton(varFrame,text="Lines",variable=self.pathfinding_mode,value=1).pack(anchor=W)
+        pmode_button_chains = Radiobutton(varFrame,text="Chains",variable=self.pathfinding_mode,value=2).pack(anchor=W)
         
         distvar_limit_label = Label(varFrame, text="Distance Limit:", anchor=W).pack()
-        distvar_limit_field = Entry(varFrame, width=5).pack()
+        self.distvar_limit_field = Entry(varFrame, width=5,textvariable=self.distance_limit).pack()
         
         angle_limit_label = Label(varFrame, text="Angle Limit:", anchor=W).pack()
-        angle_limit_field = Entry(varFrame, width=5).pack()
+        self.angle_limit_field = Entry(varFrame, width=5,textvariable=self.angle_limit).pack()
         
         length_limit_label = Label(varFrame, text="Min Length:", anchor=W).pack()
-        length_limit_field = Entry(varFrame, width=5).pack()
+        self.length_limit_field = Entry(varFrame, width=5,textvariable=self.min_line_length).pack()
         
         distance_weight_label = Label(varFrame, text="Distance Weight:", anchor=W).pack()
-        distance_weight_field = Entry(varFrame, width=5).pack()
+        self.distance_weight_field = Entry(varFrame, width=5,textvariable=self.dist_weight).pack()
         
         distvar_weight_label = Label(varFrame, text="Distance Var Weight:", anchor=W).pack()
-        distvar_weight_field = Entry(varFrame, width=5).pack()
+        self.distvar_weight_field = Entry(varFrame, width=5,textvariable=self.distvar_weight).pack()
         
         angle_weight_label = Label(varFrame, text="Angle Weight:", anchor=W).pack()
-        length_weight_field = Entry(varFrame, width=5).pack()
+        self.length_weight_field = Entry(varFrame, width=5,textvariable=self.anglevar_weight).pack()
         
         #viewport
         viewport = Frame(frame,relief=SUNKEN,borderwidth=2,width=500,height=500)
@@ -72,11 +89,21 @@ class PlaygroundWindow:
 
 
     def research(self):
+        params = CParams(eval(self.distance_limit.get()),
+                         eval(self.angle_limit.get()),
+                         eval(self.min_line_length.get()),
+                         eval(self.anglevar_weight.get()),
+                         eval(self.distvar_weight.get()),
+                         eval(self.dist_weight.get()),
+                         self.pathfinding_mode
+                         )
+        print params
         self.c.delete("line")
         searchMe = []
         for o in self.c.find_all():
             searchMe.append(PhysicalObject(o,np.array(self.c.coords(o)),0,0))
-        results = chainfinder.findChains(searchMe)
+        results = chainfinder.findChains(searchMe,params.distance_limit,params.angle_limit,params.min_line_length,
+                                         params.anglevar_weight,params.distvar_weight,params.dist_weight,params.mode)
         if len(results)>0:
             self.chainViz(results)
             
